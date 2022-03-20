7b82b6b4a4a987385b3fa55596ae674b15a06a98
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-03-20 14:48:15 +0200

    Solve 141. Linked List Cycle

---
+++ src/main/java/com/yyatsiuk/codingpatterns/fast_and_slow_pointers/LinkedListCycle.java
@@ -0,0 +1,59 @@
+package com.yyatsiuk.codingpatterns.fast_and_slow_pointers;
+
+/**
+ * <a href="https://leetcode.com/problems/linked-list-cycle/">141. Linked List Cycle</a>
+ * <p>
+ * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+ * There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+ * Internally, pos is used to denote the index of the node that tail's next pointer is connected to.
+ * Note that pos is not passed as a parameter.
+ * Return true if there is a cycle in the linked list. Otherwise, return false.
+ */
+public class LinkedListCycle {
+
+    private static class ListNode {
+        private int value = 0;
+        private ListNode next;
+
+        ListNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+
+        head.next.next.next.next.next.next = head.next.next;
+        System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+
+        head.next.next.next.next.next.next = head.next.next.next;
+        System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+    }
+
+}
