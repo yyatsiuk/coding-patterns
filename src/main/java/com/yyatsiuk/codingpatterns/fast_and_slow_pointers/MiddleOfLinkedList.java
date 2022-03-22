001a6eaca5688a66cf983c5482bcca96a4f924a1
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-03-22 10:27:31 +0200

    Solve 876. Middle of the Linked List

---
+++ src/main/java/com/yyatsiuk/codingpatterns/fast_and_slow_pointers/MiddleOfLinkedList.java
@@ -0,0 +1,57 @@
+package com.yyatsiuk.codingpatterns.fast_and_slow_pointers;
+
+/**
+ * <a href="876. Middle of the Linked List">876. Middle of the Linked List</a>
+ * <p>
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ * If there are two middle nodes, return the second middle node.
+ * <p>
+ * <b>Example 1:</b>
+ * Input: head = [1,2,3,4,5]
+ * Output: [3,4,5]
+ * Explanation: The middle node of the list is node 3.
+ * <p>
+ * <b>Example 2:</b>
+ * Input: head = [1,2,3,4,5,6]
+ * Output: [4,5,6]
+ * Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ */
+public class MiddleOfLinkedList {
+
+    private static class ListNode {
+        int value = 0;
+        ListNode next;
+
+        ListNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public static ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println("Middle Node: " + MiddleOfLinkedList.findMiddle(head).value);
+
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println("Middle Node: " + MiddleOfLinkedList.findMiddle(head).value);
+
+        head.next.next.next.next.next.next = new ListNode(7);
+        System.out.println("Middle Node: " + MiddleOfLinkedList.findMiddle(head).value);
+    }
+
+}
