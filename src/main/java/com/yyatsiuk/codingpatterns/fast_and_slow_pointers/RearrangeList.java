3b7ca7fc317e4560d2b39d548bf7b33c88f01e87
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-03-22 18:32:11 +0200

    Solve 143. Reorder List

---
+++ src/main/java/com/yyatsiuk/codingpatterns/fast_and_slow_pointers/RearrangeList.java
@@ -0,0 +1,82 @@
+package com.yyatsiuk.codingpatterns.fast_and_slow_pointers;
+
+/**
+ * <a href="https://leetcode.com/problems/reorder-list/">143. Reorder List</a>
+ * <p>
+ * Given the head of a Singly LinkedList, write a method to modify the LinkedList such that the nodes from the second half of the LinkedList
+ * are inserted alternately to the nodes from the first half in reverse order.
+ * So if the LinkedList has nodes 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null,
+ * your method should return 1 -> 6 -> 2 -> 5 -> 3 -> 4 -> null.
+ * <p>
+ * <b>Example 1:</b>
+ * Input: 2 -> 4 -> 6 -> 8 -> 10 -> 12 -> null
+ * Output: 2 -> 12 -> 4 -> 10 -> 6 -> 8 -> null
+ * <p>
+ * <b>Example 2:</b>
+ * Input: 2 -> 4 -> 6 -> 8 -> 10 -> null
+ * Output: 2 -> 10 -> 4 -> 8 -> 6 -> null
+ */
+@SuppressWarnings("Duplicates")
+public class RearrangeList {
+
+    private static class ListNode {
+        int value;
+        ListNode next;
+
+        ListNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void reorder(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode first = head;
+        ListNode second = reverse(slow);
+        while (second.next != null) {
+            ListNode tmp = first.next;
+            first.next = second;
+            first = tmp;
+
+            tmp = second.next;
+            second.next = first;
+            second = tmp;
+        }
+    }
+
+    private static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(2);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(6);
+        head.next.next.next = new ListNode(8);
+        head.next.next.next.next = new ListNode(10);
+        head.next.next.next.next.next = new ListNode(12);
+        RearrangeList.reorder(head);
+        while (head != null) {
+            System.out.print(head.value + " ");
+            head = head.next;
+        }
+    }
+
+}
+
