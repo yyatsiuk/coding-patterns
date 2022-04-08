089ed9b6f276844c2ecc85e67dd4e61cc1dd5cb8
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-04-08 22:33:11 +0300

    Solve 111. Minimum Depth of Binary Tree

---
+++ src/main/java/com/yyatsiuk/codingpatterns/bredth_first_search/MinimumBinaryTreeDepth.java
@@ -0,0 +1,61 @@
+package com.yyatsiuk.codingpatterns.bredth_first_search;
+
+import com.yyatsiuk.codingpatterns.bredth_first_search.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * <a href="https://leetcode.com/problems/minimum-depth-of-binary-tree/">111. Minimum Depth of Binary Tree</a>
+ * <p>
+ * Find the minimum depth of a binary tree.
+ * The minimum depth is the number of nodes along the shortest path from the root node to the nearest leaf node.
+ * <p>
+ * <b>Example 1:</b>
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 2
+ * <p>
+ * <b>Example 2:</b>
+ * Input: root = [2,null,3,null,4,null,5,null,6]
+ * Output: 5
+ */
+public class MinimumBinaryTreeDepth {
+
+    public static int findDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int minimumTreeDepth = 0;
+        while (!queue.isEmpty()) {
+            minimumTreeDepth++;
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.remove();
+
+                if (currentNode.left == null && currentNode.right == null)
+                    return minimumTreeDepth;
+
+                if (currentNode.left != null)
+                    queue.add(currentNode.left);
+                if (currentNode.right != null)
+                    queue.add(currentNode.right);
+            }
+        }
+        return minimumTreeDepth;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        System.out.println("Tree Minimum Depth: " + MinimumBinaryTreeDepth.findDepth(root));
+        root.left.left = new TreeNode(9);
+        root.right.left.left = new TreeNode(11);
+        System.out.println("Tree Minimum Depth: " + MinimumBinaryTreeDepth.findDepth(root));
+    }
+
+}
