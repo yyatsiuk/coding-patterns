8b1609aa4fa27edf0f48de2a34ee494704b1b27f
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-04-15 19:51:25 +0300

    Solve 46. Permutations

---
+++ src/main/java/com/yyatsiuk/codingpatterns/subsets/Permutations.java
@@ -0,0 +1,42 @@
+package com.yyatsiuk.codingpatterns.subsets;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * <a href="https://leetcode.com/problems/permutations/">46. Permutations</a>
+ */
+public class Permutations {
+
+    public static List<List<Integer>> findPermutations(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<List<Integer>> permutations = new LinkedList<>();
+        permutations.add(new ArrayList<>());
+
+        for (int num : nums) {
+            int n = permutations.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> oldPermutation = permutations.poll();
+                for (int j = 0; j <= oldPermutation.size(); j++) {
+                    List<Integer> newPermutation = new ArrayList<>(oldPermutation);
+                    newPermutation.add(j, num);
+                    if (newPermutation.size() == nums.length) {
+                        result.add(newPermutation);
+                    } else {
+                        permutations.add(newPermutation);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> result = Permutations.findPermutations(new int[]{1, 3, 5});
+        System.out.print("Here are all the permutations: " + result);
+    }
+
+}
