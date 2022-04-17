519d09a8accfeb0dedef2108cde833576334fd3c
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-04-17 14:15:51 +0300

    Solve 852. Peak Index in a Mountain Array

---
+++ src/main/java/com/yyatsiuk/codingpatterns/binary_search/MaxInBitonicArray.java
@@ -0,0 +1,25 @@
+package com.yyatsiuk.codingpatterns.binary_search;
+
+/**
+ * <a href="https://leetcode.com/problems/peak-index-in-a-mountain-array/">852. Peak Index in a Mountain Array</a>
+ */
+public class MaxInBitonicArray {
+
+
+    public int peakIndexInMountainArray(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low < high) {
+            int mid = (low + high) >>> 1;
+            if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+}
