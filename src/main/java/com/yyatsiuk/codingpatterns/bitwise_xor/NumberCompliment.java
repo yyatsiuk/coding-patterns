b6408827064bd20e3f3289237dcc114f43a6c5ab
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-04-21 10:32:30 +0300

    Solve 476. Number Complement

---
+++ src/main/java/com/yyatsiuk/codingpatterns/bitwise_xor/NumberCompliment.java
@@ -0,0 +1,24 @@
+package com.yyatsiuk.codingpatterns.bitwise_xor;
+
+/**
+ * <a href="https://leetcode.com/problems/number-complement/">476. Number Complement</a>
+ */
+public class NumberCompliment {
+
+    public static int findComplement(int num) {
+         if (num == 0) return 1;
+        int todo = num, bit = 1;
+        while (todo != 0) {
+            num = num ^ bit;
+            bit = bit << 1;
+            todo = todo >> 1;
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("findComplement(5) = " + findComplement(5));
+        System.out.println("findComplement(150) = " + findComplement(150));
+    }
+
+}
