de979d37833470d619d75dcfccb5cc8d63c01d2d
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-04-23 10:13:40 +0300

    Solve 215. Kth Largest Element in an Array

---
+++ src/main/java/com/yyatsiuk/codingpatterns/top_k/KLargestNumbers.java
@@ -0,0 +1,25 @@
+package com.yyatsiuk.codingpatterns.top_k;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * <a href="https://leetcode.com/problems/kth-largest-element-in-an-array/">215. Kth Largest Element in an Array</a>
+ */
+public class KLargestNumbers {
+
+    public int findKthLargest(int[] nums, int k) {
+        Queue<Integer> heap = new PriorityQueue<>(k);
+        for (int num : nums) {
+            if (heap.size() < k) {
+                heap.add(num);
+            } else if (heap.peek() < num) {
+                heap.poll();
+                heap.add(num);
+            }
+        }
+
+        return heap.poll();
+    }
+
+}
