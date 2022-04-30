a3a43954a9962ae2f42568f41ba313ab71a0188a
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-04-30 11:35:58 +0300

    Solve 100. Same Tree

---
+++ src/main/java/com/yyatsiuk/codingpatterns/depth_first_search/IsSameTree.java
@@ -0,0 +1,17 @@
+package com.yyatsiuk.codingpatterns.depth_first_search;
+
+import com.yyatsiuk.codingpatterns.common.TreeNode;
+
+/**
+ * <a href="https://leetcode.com/problems/same-tree/">100. Same Tree</a>
+ */
+public class IsSameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (q == null || p == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+    }
+
+}
