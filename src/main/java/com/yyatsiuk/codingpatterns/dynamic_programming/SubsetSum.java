3d09dd34ba86ee62b36434c31da66d9994e2cfa5
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-05-19 11:22:58 +0300

    Solve Subset Sum (medium)

---
+++ src/main/java/com/yyatsiuk/codingpatterns/dynamic_programming/SubsetSum.java
@@ -0,0 +1,30 @@
+package com.yyatsiuk.codingpatterns.dynamic_programming;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <a href="https://www.educative.io/courses/grokking-the-coding-interview/gxrnL0GQGqk">Subset Sum (medium)</a>
+ */
+public class SubsetSum {
+
+    static boolean canPartition(int[] nums, int sum) {
+        if (nums == null || nums.length <= 1) return false;
+
+        Set<Integer> set = new HashSet<>();
+        set.add(0);
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int currentNum = nums[i];
+            Set<Integer> nextSet = new HashSet<>();
+            for (Integer subsetSum : set) {
+                nextSet.add(subsetSum);
+                nextSet.add(subsetSum + currentNum);
+                if (nextSet.contains(sum)) return true;
+            }
+            set = nextSet;
+        }
+
+        return false;
+    }
+
+}
