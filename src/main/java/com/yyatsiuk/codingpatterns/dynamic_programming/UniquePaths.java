1e7a3759abdb10e41b82caa791dd8b83aeb714b0
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-05-30 21:40:49 +0300

    Solve: 62. Unique Paths

---
+++ src/main/java/com/yyatsiuk/codingpatterns/dynamic_programming/UniquePaths.java
@@ -0,0 +1,27 @@
+package com.yyatsiuk.codingpatterns.dynamic_programming;
+
+import java.util.Arrays;
+
+public class UniquePaths {
+
+    /**
+     * Time complexity: O(N×M)
+     * Space complexity: O(N×M)
+     *
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] d = new int[m][n];
+
+        for (int[] arr : d) {
+            Arrays.fill(arr, 1);
+        }
+
+        for (int col = 1; col < m; ++col) {
+            for (int row = 1; row < n; ++row) {
+                d[col][row] = d[col - 1][row] + d[col][row - 1];
+            }
+        }
+        return d[m - 1][n - 1];
+    }
+
+}
