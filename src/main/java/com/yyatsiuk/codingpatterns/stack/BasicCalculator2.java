47ab0918528d80588ebc42763d76077dc5c27628
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-06-17 08:55:43 +0300

    Solve 227. Basic Calculator II

--- src/main/java/com/yyatsiuk/codingpatterns/stack/BasicCalculator2.java
+++ src/main/java/com/yyatsiuk/codingpatterns/stack/BasicCalculator2.java
@@ -1,22 +1,49 @@
 package com.yyatsiuk.codingpatterns.stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * <a href="https://leetcode.com/problems/basic-calculator-ii/">227. Basic Calculator II</a>
+ */
 public class BasicCalculator2 {
 
     public int calculate(String s) {
-        s = s.replaceAll(" ", "");
-        s = s.replace("+", " + ")
-                .replace("-", " - ")
-                .replace("*", " * ")
-                .replace("/", " / ");
+        Deque<Integer> stack = new ArrayDeque<>();
+        char operation = '+';
+        int currentNumber = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                currentNumber = currentNumber * 10 + (ch - '0');
+            }
 
-        String[] tokens = s.split(" ");
-        return 1;
+            if (!Character.isDigit(ch) && !Character.isWhitespace(ch) || i == s.length() - 1) {
+                if (operation == '+') {
+                    stack.push(currentNumber);
+                } else if (operation == '-') {
+                    stack.push(-currentNumber);
+                } else if (operation == '/') {
+                    stack.push(stack.pop() / currentNumber);
+                } else if (operation == '*') {
+                    stack.push(stack.pop() * currentNumber);
+                }
+
+                operation = ch;
+                currentNumber = 0;
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            currentNumber += stack.pop();
+        }
+
+        return currentNumber;
     }
-
 
     public static void main(String[] args) {
         BasicCalculator2 basicCalculator2 = new BasicCalculator2();
-        int calculate = basicCalculator2.calculate("1 *   2 + 5 / 2");
+        int calculate = basicCalculator2.calculate("2+3*2-2-0");
         System.out.println(calculate);
     }
 
