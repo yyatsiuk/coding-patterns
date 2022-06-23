af074f42be458d5195f0c57d37144e33d0a0be80
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-06-23 23:02:23 +0300

    Solve 226. Invert Binary Tree

---
+++ src/main/java/com/yyatsiuk/codingpatterns/depth_first_search/InvertBinaryTree.java
@@ -0,0 +1,22 @@
+package com.yyatsiuk.codingpatterns.depth_first_search;
+
+import com.yyatsiuk.codingpatterns.common.TreeNode;
+
+/**
+ * <a href="https://leetcode.com/problems/invert-binary-tree/">226. Invert Binary Tree</a>
+ */
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+
+}
