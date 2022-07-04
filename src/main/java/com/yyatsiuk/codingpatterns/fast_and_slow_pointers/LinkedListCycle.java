9111b0c7db049ccf5752fc5ea7b0c3480af2726c
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-07-04 11:21:50 +0300

    Update 141. Linked List Cycle solution

--- src/main/java/com/yyatsiuk/codingpatterns/fast_and_slow_pointers/LinkedListCycle.java
+++ src/main/java/com/yyatsiuk/codingpatterns/fast_and_slow_pointers/LinkedListCycle.java
@@ -21,22 +21,17 @@ public class LinkedListCycle {
     }
 
     public static boolean hasCycle(ListNode head) {
-        if (head == null) {
-            return false;
-        }
+        if (head == null) return false;
 
         ListNode slow = head;
-        ListNode fast = head.next;
-        while (slow != fast) {
-            if (fast == null || fast.next == null) {
-                return false;
-            }
-
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+            if (slow == fast) return true;
         }
 
-        return true;
+        return false;
     }
 
 
