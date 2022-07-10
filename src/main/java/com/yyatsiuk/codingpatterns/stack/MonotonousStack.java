9c992c4ac8e52391ce2922c2f8e894e794cb5c27
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-07-10 09:58:02 +0300

    Solve 503. Next Greater Element II

---
+++ src/main/java/com/yyatsiuk/codingpatterns/stack/MonotonousStack.java
@@ -0,0 +1,39 @@
+package com.yyatsiuk.codingpatterns.stack;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+/**
+ * <a href="https://leetcode.com/problems/next-greater-element-ii/">503. Next Greater Element II</a>
+ */
+public class MonotonousStack {
+
+    public int[] nextGreaterElements(int[] arr) {
+        int[] nextGrater = new int[arr.length];
+        Arrays.fill(nextGrater, -1);
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                nextGrater[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                nextGrater[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        return nextGrater;
+    }
+
+    public static void main(String[] args) {
+        MonotonousStack monotonousStack = new MonotonousStack();
+        int[] ints = monotonousStack.nextGreaterElements(new int[]{1, 2, 1});
+        System.out.println(Arrays.toString(ints));
+    }
+}
