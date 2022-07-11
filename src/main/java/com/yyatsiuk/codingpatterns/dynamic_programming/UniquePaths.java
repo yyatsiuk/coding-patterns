78d77d59a65218a33c59eac028da9e069089be52
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-07-11 13:59:52 +0300

    Add top-down solution for the 62. Unique Paths problem

--- src/main/java/com/yyatsiuk/codingpatterns/dynamic_programming/UniquePaths.java
+++ src/main/java/com/yyatsiuk/codingpatterns/dynamic_programming/UniquePaths.java
@@ -2,12 +2,14 @@ package com.yyatsiuk.codingpatterns.dynamic_programming;
 
 import java.util.Arrays;
 
+/**
+ * <a href="https://leetcode.com/problems/unique-paths/>62. Unique Paths</a>
+ */
 public class UniquePaths {
 
     /**
      * Time complexity: O(N×M)
      * Space complexity: O(N×M)
-     *
      */
     public int uniquePaths(int m, int n) {
         int[][] d = new int[m][n];
@@ -22,6 +24,28 @@ public class UniquePaths {
             }
         }
         return d[m - 1][n - 1];
+    }
+
+    public int uniquePaths2(int m, int n) {
+        Integer[][] memo = new Integer[m][n];
+        return uniquePathsTopDown(0, 0, memo, m, n);
+    }
+
+    private int uniquePathsTopDown(int row, int col, Integer[][] memo, int m, int n) {
+        if (row == (m - 1) && col == (n - 1)) {
+            return 1;
+        }
+        if (row >= m || col >= n) {
+            return 0;
+        }
+
+        if (memo[row][col] != null) {
+            return memo[row][col];
+        }
+
+        memo[row][col] = uniquePathsTopDown(row + 1, col, memo, m, n) + uniquePathsTopDown(row, col + 1, memo, m, n);
+
+        return memo[row][col];
     }
 
 }
