f347c5725743b94deb982233b2e77e2ddecaff51
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-07-14 23:41:58 +0300

    Solve 907. Sum of Subarray Minimums

---
+++ src/main/java/com/yyatsiuk/codingpatterns/monotonous_stack/SumOfSubarrayMinimums.java
@@ -0,0 +1,50 @@
+package com.yyatsiuk.codingpatterns.monotonous_stack;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+/**
+ * <a href="https://leetcode.com/problems/sum-of-subarray-minimums/">907. Sum of Subarray Minimums</a>
+ */
+public class SumOfSubarrayMinimums {
+
+    private static final int MOD = 1000000007;
+
+    public int sumSubarrayMins(int[] arr) {
+        final int n = arr.length;
+        int[] prevLess = new int[n];
+        int[] nextLess = new int[n];
+
+        Arrays.fill(nextLess, n);
+
+        Deque<Integer> monoStack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            while (!monoStack.isEmpty() && arr[monoStack.peek()] >= arr[i]) {
+                nextLess[monoStack.pop()] = i;
+            }
+
+            prevLess[i] = monoStack.isEmpty() ? -1 : monoStack.peek();
+            monoStack.push(i);
+        }
+
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            long left = i - prevLess[i];
+            long right = nextLess[i] - i;
+
+            sum += arr[i] * left * right;
+            sum %= MOD;
+        }
+
+        return (int) (sum % MOD);
+    }
+
+
+    public static void main(String[] args) {
+        SumOfSubarrayMinimums test = new SumOfSubarrayMinimums();
+        int result = test.sumSubarrayMins(new int[]{3, 1, 2, 4});
+        System.out.println(result);
+    }
+
+}
