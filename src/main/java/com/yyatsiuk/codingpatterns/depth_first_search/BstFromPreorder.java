444ff273047f2383c4cbcf467e46575e90ec9151
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-07-17 09:19:58 +0300

    Add monotonic stack solution to the 1008. Construct Binary Search Tree from Preorder Traversal

--- src/main/java/com/yyatsiuk/codingpatterns/depth_first_search/BstFromPreorder.java
+++ src/main/java/com/yyatsiuk/codingpatterns/depth_first_search/BstFromPreorder.java
@@ -2,6 +2,9 @@ package com.yyatsiuk.codingpatterns.depth_first_search;
 
 import com.yyatsiuk.codingpatterns.common.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * <a href="https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/">1008. Construct Binary Search Tree from Preorder Traversal</a>
  */
@@ -18,6 +21,28 @@ public class BstFromPreorder {
         TreeNode root = new TreeNode(preorder[i++]);
         root.left = bstFromPreorder(preorder, root.val);
         root.right = bstFromPreorder(preorder, bound);
+        return root;
+    }
+
+    public TreeNode bstFromPreorderMonotonicStack(int[] preorder) {
+        int n = preorder.length;
+        if (n == 0) return null;
+
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.push(root);
+
+        for (int i = 1; i < n; i++) {
+            TreeNode node = deque.peek();
+            TreeNode child = new TreeNode(preorder[i]);
+            while (!deque.isEmpty() && deque.peek().val < child.val)
+                node = deque.pop();
+
+            if (node.val < child.val) node.right = child;
+            else node.left = child;
+            deque.push(child);
+        }
+
         return root;
     }
 
