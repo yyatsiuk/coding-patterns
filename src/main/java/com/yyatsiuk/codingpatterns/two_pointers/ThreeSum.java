35856dfe11ed6cb8e6f857603af0a5c4279b4255
yyatsiuk <yatsyuk.yaroslav@gmail.com>
2022-07-28 21:54:53 +0300

    Update 15. 3Sum Solution

--- src/main/java/com/yyatsiuk/codingpatterns/two_pointers/ThreeSum.java
+++ src/main/java/com/yyatsiuk/codingpatterns/two_pointers/ThreeSum.java
@@ -13,73 +13,67 @@ import java.util.Set;
 
 /**
  * <a href="https://leetcode.com/problems/3sum/">15. 3Sum</a>
- * <p>
- * Given an integer array nums, return all the triplets
- * Notice that the solution set must not contain duplicate triplets.
- * <p>
- * <b>Example 1:</b>
- * Input: [-3, 0, 1, 2, -1, 1, -2]
- * Output: [-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]
- * Explanation: There are four unique triplets whose sum is equal to zero.
- * <p>
- * <b>Example 2:</b>
- * Input: [-5, 2, -1, -2, 3]
- * Output: [[-5, 2, 3], [-2, -1, 3]]
- * Explanation: There are two unique triplets whose sum is equal to zero.
  */
 @Slf4j
 public class ThreeSum {
 
-    public static List<List<Integer>> searchTriplets(int[] nums) {
-        List<List<Integer>> triplets = new ArrayList<>();
+    public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i - 1] == nums[i]) continue;
-
-            int compliment = nums[i] * -1;
-            int left = i + 1;
-            int right = nums.length - 1;
-            while (left < right) {
-                if (nums[left] + nums[right] == compliment) {
-                    triplets.add(List.of(nums[left], nums[right], nums[i]));
-                    left++;
-                    right--;
-
-                    while (left < nums.length && nums[left - 1] == nums[left]) left++;
-                    while (right >= 0 && nums[right + 1] == nums[right]) right--;
-                } else if (nums[left] + nums[right] > compliment) right--;
-                else left++;
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                twoSum(i, nums, result);
             }
         }
-        return triplets;
+
+        return result;
     }
 
-    public List<List<Integer>> threeSum(int[] nums) {
-        Set<Integer> dups = new HashSet<>();
-        Map<Integer, Integer> seen = new HashMap<>();
-        Set<List<Integer>> res = new HashSet<>();
+    private void twoSum(int currIndex, int[] nums, List<List<Integer>> result) {
+        int left = currIndex + 1;
+        int right = nums.length - 1;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (dups.add(nums[i])) {
+        while (left < right) {
+            int sum = nums[left] + nums[right] + nums[currIndex];
+            if (sum < 0) {
+                left++;
+            } else if (sum > 0) {
+                right--;
+            } else {
+                result.add(List.of(nums[left], nums[currIndex], nums[right]));
+                left++;
+                right--;
+                while (left < right && nums[left] == nums[left - 1]) {
+                    left++;
+                }
+            }
+        }
+    }
+
+    public List<List<Integer>> threeSumNoSort(int[] nums) {
+        Set<List<Integer>> res = new HashSet<>();
+        Set<Integer> duplicates = new HashSet<>();
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; i++)
+            if (duplicates.add(nums[i])) {
                 for (int j = i + 1; j < nums.length; j++) {
-                    int num = -nums[i] - nums[j];
-                    if (seen.containsKey(num) && seen.get(num) == 1) {
-                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], num);
+                    int complement = -(nums[i] + nums[j]);
+                    if (seen.containsKey(complement) && seen.get(complement) == i) {
+                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
                         Collections.sort(triplet);
                         res.add(triplet);
                     }
                     seen.put(nums[j], i);
                 }
             }
-        }
-
         return new ArrayList<>(res);
     }
 
 
     public static void main(String[] args) {
-        log.info("Result = {}", ThreeSum.searchTriplets(new int[]{-3, 0, 1, 2, -1, 1, -2}));
-        log.info("Result = {}", ThreeSum.searchTriplets(new int[]{-5, 2, -1, -2, 3}));
+        ThreeSum test = new ThreeSum();
+        log.info("Result = {}", test.threeSum(new int[]{-3, 0, 1, 2, -1, 1, -2}));
+        log.info("Result = {}", test.threeSumNoSort(new int[]{-5, 2, -1, -2, 3}));
     }
 
 }
